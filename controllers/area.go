164c1c6c5ca91ccb169d91d39de4adf648a8ca83
200637086@qq.com <200637086@qq.com>
2018-10-19 23:37:39 +0800

    redis配置成功

--- controllers/area.go
+++ controllers/area.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/cache"
+	_ "github.com/astaxie/beego/cache/redis"
 	"github.com/astaxie/beego/orm"
 	"loveHome/models"
+	"time"
 )
 
 type AreaController struct {
@@ -20,9 +23,16 @@ func (c *AreaController) GetArea() {
 	resp := make(map[string]interface{})
 	//打包成json格式，返回给客户端
 	defer c.RetData(resp)
-	//从session获取数据
-
-	//从数据库获取数据
+	//从redis缓存中读取数据
+	bm, errConn := cache.NewCache("redis", `{"key":"lovehome","conn":"47.94.213.158:6379","dbNum":"0"}`)
+	if errConn != nil {
+		beego.Info("cache.NewCache err:", errConn)
+	}
+	cacheErr := bm.Put("aaa", "bbb", 10*time.Second)
+	if cacheErr != nil {
+		beego.Error("bm.Put err:", cacheErr)
+	}
+	//从mysql数据库获取数据
 	area := []models.Area{} //定义一个接收数据对对象
 	o := orm.NewOrm()       //获取连接
 	n, err := o.QueryTable("area").All(&area)
